5b7599bbb36b2186e2002d693269d923e1c82d90
Andrei <andrdru@gmail.com>
2016-12-19 17:24:07 +0300

    update postgresql enviroment

--- src/testdb.php
+++ src/testdb.php
@@ -1,5 +1,5 @@
 <?php
-$dbconn = pg_connect("host=db port=5432 user=postgres password=postgres");
+$dbconn = pg_connect("host=db port=5432 dbname=myappdb user=admin password=pleasechangeme");
 
 if($dbconn)
 {
